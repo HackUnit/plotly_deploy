e0996986b749028371459f6847fbace3b4c1d0c4
HackUnit <j.larose26@gmail.com>
2022-08-31 21:27:57 -0500

    Modified plot.js to show all demographic info for each ID selected

--- plot.js
+++ plot.js
@@ -90,18 +90,20 @@ function buildMetadata(sample) {
     d3.json("samples.json").then((data) => {
 
         var metadata = data.metadata;
-
+        // Filter the data for the object with the desired sample number
         var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
-
         var result = resultArray[0];
-        
+        // Use d3 to select the panel with the id of '#sample-metadata'
         var PANEL = d3.select("#sample-metadata");
 
-
+        // Use '.html("") to clear any existing metadata
         PANEL.html("");
 
-        PANEL.append("h6").text(result.location);
 
+        Object.entries(result).forEach(([key, value]) => {
+
+            PANEL.append("h6").text(`${key.toUpperCase()}: ${value}`);
+        });
     });
 }
 
